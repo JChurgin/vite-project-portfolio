db8303b8e22bda82b2ad49b1051102994e5df1f5
JChurgin <114018853+JChurgin@users.noreply.github.com>
2023-06-15 22:36:28 +0300

    add: deploy workflow

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./experience.css";
-import { BsPatchCheckFill } from "react-icons/bs";
+import { BsPatchCheckFill } from "react-icons/Bs";
 
 const Experience = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-project-protfolio/"
+  base: "/vite-project-protfolio/",
+  build: {
+    rollupOptions: {
+      external: ['react-icons/Bs']
+    }
+  }
 })
 
