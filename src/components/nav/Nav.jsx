7e70dab902940d1dfee88008c38b165eecc0603f
JChurgin <114018853+JChurgin@users.noreply.github.com>
2023-06-18 16:21:37 +0300

    redeploy7

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css';
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
 import { BiBook } from 'react-icons/bi'
-import { BsFileEarmarkCode } from 'react-icons/Bs'
+import { BiCodeAlt } from 'react-icons/bi'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 
 
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#recommendation" onClick={() => setActiveNav('#recommendation')} className={activeNav === '#recommendation' ? 'active' : ''}><BiBook/></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsFileEarmarkCode/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeAlt/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
